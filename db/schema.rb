411fda3932054eee8627e2e1ce31d0273df83011
carla <carlaibelc@hotmail.com>
2023-02-01 13:37:27 -0300

    correccion de formato

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_01_145033) do
+ActiveRecord::Schema[7.0].define(version: 20_230_201_145_033) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name"
-    t.string "lastname"
-    t.string "email"
+  create_table 'users', force: :cascade do |t|
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'name'
+    t.string 'lastname'
+    t.string 'email'
   end
-
 end
