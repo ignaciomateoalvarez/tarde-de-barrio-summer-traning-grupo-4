0ac0e20369195553cfd2711db934f5f8d3fefa8a
carla <carlaibelc@hotmail.com>
2023-02-01 13:21:15 -0300

    validaciones

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+
+  validates :name, :lastname, :email, presence: true
+  validates_format_of :name, :lastname, { with: /\A[a-zA-Z]+\z/ }
+  #validates :lastname, presence: true
+  #validates_format_of :lastname, { with: /\A[a-zA-Z]+\z/ }
+  validates :email, uniqueness: true
+  validates_format_of :email, { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+
+
 end
+
