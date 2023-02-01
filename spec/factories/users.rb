aaf0795348607afe3f0337e6379ba1bf8ac58e8a
carla <carlaibelc@hotmail.com>
2023-02-01 16:11:16 -0300

    tests

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :user do
+    name { Faker::Name.first_name }
+    lastname { Faker::Name.last_name }
+    email { Faker::Internet.unique.email }
+  end
+end
