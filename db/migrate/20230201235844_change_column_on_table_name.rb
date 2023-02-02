5e9d159cedfa64dee3c055ce6d653ad0bc7f0114
carla <carlaibelc@hotmail.com>
2023-02-02 11:36:10 -0300

    creat tabla users con sorcery

--- db/migrate/20230201235844_change_column_on_table_name.rb
+++
@@ -1,7 +0,0 @@
-class ChangeColumnOnTableName < ActiveRecord::Migration[7.0]
-  def change
-    change_column_null :users, :name, null: false
-    change_column_null :users, :lastname, null: false
-    change_column_null :users, :email, null: false
-  end
-end
