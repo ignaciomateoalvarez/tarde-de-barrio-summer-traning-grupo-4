13f3a76764eae586f953abd0544e2fb3ff36be7d
carla <carlaibelc@hotmail.com>
2023-02-02 15:36:54 -0300

    conexion sign in con sorcery

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,22 @@ class SessionsController < ApplicationController
 
   def new; end
 
-  def create; end
+  def create
+    @user = login(params[:email], params[:password])
+
+    if @user
+      redirect_back_or_to(homes_path, notice: 'Login successful')
+    else
+      flash.now[:alert] = 'Login failed'
+      render action: :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if logout
+      redirect_to(root_path, notice: 'Logged out!')
+    else
+      flash.now[:error] = 'Error'
+    end
+  end
 end
