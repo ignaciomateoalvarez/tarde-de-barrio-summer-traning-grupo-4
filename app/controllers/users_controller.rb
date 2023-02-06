c9ce6366240cda68879051a3d3f7c8370ba35461
CamilaTisera <camilanoemitisera@gmail.com>
2023-02-06 12:38:08 -0300

    CreateUser -todavia no funciona

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,14 +24,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to user_url(@user), notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      redirect_to @user
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -66,7 +62,7 @@ class UsersController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def user_params
-    params.require(:user).permit(:name, :lastname, :email, :password, :password_confirmation)
+    params.require(:user).permit!
   end
 
   def filter_params
