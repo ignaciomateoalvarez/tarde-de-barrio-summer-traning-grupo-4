0e88ec7bcecefc98908477462449c36473407dbd
carla <carlaibelc@hotmail.com>
2023-02-06 14:05:06 -0300

    registrar usuario

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,13 @@
 Rails.application.routes.draw do
-  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 
   root 'sessions#new'
   resources :registrations
   resources :sessions
-
-  # root :to => 'users#index'
-  resources :users
+  resources :users 
 
   resources :homes, only: [:index]
-
   get 'logout' => 'sessions#destroy', :as => 'logout'
 end
