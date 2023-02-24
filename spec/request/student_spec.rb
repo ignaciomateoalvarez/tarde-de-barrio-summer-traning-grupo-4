8dd698778a929a43300b0262265f1b738d8ce775
lautaromastronardi <lautaromastronardi@gmail.com>
2023-02-24 12:35:33 -0300

    Test de estudiantes agregados

--- spec/request/student_spec.rb
+++ spec/request/student_spec.rb
@@ -2,36 +2,10 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Student Routes', type: :request do
-  describe 'Create student' do
-    let!(:user) { login_user }
-    let!(:student) { build(:student) }
-    before { post students_path(:student) }
-    context 'When user is logged' do
-      it 'returns http success' do
-        expect(response).to have_http_status(:ok)
-        expect(response).to render_template(:index)
-      end
-    end
-  end
-end
-
 RSpec.describe 'StudentController', type: :request do
   before(:each) do
     @user = build(:user)
     login_user
-  end
-
-  describe 'Create student' do
-    let!(:user) { login_user }
-    let!(:student) { build(:student) }
-    before { post students_path(:student) }
-    context 'When user is logged' do
-      it 'returns http success' do
-        expect(response).to have_http_status(:ok)
-        expect(response).to render_template(:index)
-      end
-    end
   end
 
   describe 'GET index' do
@@ -45,9 +19,7 @@ RSpec.describe 'StudentController', type: :request do
 
   describe 'GET show' do
     let!(:student) { create(:student) }
-
     before { get student_path(student) }
-
     context 'when request is valid' do
       it { expect(response).to have_http_status(:ok) }
       it { expect(response).to render_template(:show) }
@@ -64,10 +36,33 @@ RSpec.describe 'StudentController', type: :request do
   end
 
   describe 'GET edit' do
-    before { get edit_student_path }
+    let!(:student) { create(:student) }
+    before { get edit_student_path(student) }
     context 'when request is valid' do
       it { expect(response).to have_http_status(:ok) }
       it { expect(response).to render_template(:edit) }
+    end
+  end
+
+  describe 'Create student' do
+    let!(:user) { login_user }
+    let!(:student) { build(:student) }
+    before { post students_path student: student.attributes }
+    context 'When user is logged' do
+      it 'returns http success' do
+        expect(response).to have_http_status(:found)
+        expect(response).to redirect_to(students_path)
+      end
+    end
+  end
+
+  describe 'Update student' do
+    let!(:student) { create(:student) }
+    it 'update student attributes' do
+      put student_path(student.id), params: { student: student.attributes }
+      student.reload
+      expect(response).to have_http_status(:found)
+      expect(response).to redirect_to(students_path)
     end
   end
 end
